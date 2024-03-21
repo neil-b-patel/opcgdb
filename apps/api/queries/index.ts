dd6b6620e318a29c5d7416522c45bb3e185f9859
Marcos Mellado <marcos.mellado@resn.co.nz>
2024-03-21 12:19:24 +0100

    feat(api): expose queries as lib

---
+++ apps/api/queries/index.ts
@@ -0,0 +1,6 @@
+export * from '../types.js';
+
+export { default as getSetById } from './getSetByID.js';
+export { default as getCardById } from './getCardByID.js';
+export { default as getCardsByFilter } from './getCardsByFilter.js';
+export { default as getCardsByNumber } from './getCardsByNumber.js';
