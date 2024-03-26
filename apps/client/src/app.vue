5d9dac6d15d35d24fd64c5f408508a5a0417664e
Marcos Mellado <marcos@mellados.com>
2024-03-26 16:06:32 +0100

    refactor: load css with postcss

--- apps/client/src/app.vue
+++ apps/client/src/app.vue
@@ -1,9 +1,6 @@
 <script setup lang="ts">
   import DatabaseProvider from '~/providers/DatabaseProvider.vue';
 
-  import './styles/reset.css';
-  import './styles/variables.css';
-
   import './styles/main.css';
 </script>
 
