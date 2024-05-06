1bb6e5dfd995da69b3f14b32bd56c9d324cb2c68
Marcos Mellado <marcos@mellados.com>
2024-05-06 14:49:30 +0200

    feat: update card detail styles

---
+++ apps/client/src/components/AttributeImage.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+  import { useRuntimeConfig } from '#imports';
+  import { computed } from 'vue';
+
+  const config = useRuntimeConfig();
+
+  const props = defineProps<{
+    attribute: string;
+  }>();
+
+  const attributeUrl = computed(() => {
+    return `${config.public.cdn_url}/cardlist/attributes/${props.attribute.toLowerCase()}.webp`;
+  });
+</script>
+
+<template>
+  <div class="wrapper">
+    <img :src="attributeUrl" alt="" :width="62" />
+    <span>{{ props.attribute }}</span>
+  </div>
+</template>
+
+<style scoped>
+  .wrapper {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+  }
+  img {
+    display: block;
+    width: var(--size-4);
+    margin-right: var(--spacing-xxs);
+  }
+
+  span {
+    text-align: center;
+  }
+</style>
--- apps/client/src/components/card/CardDetails.vue
+++ apps/client/src/components/card/CardDetails.vue
@@ -4,6 +4,7 @@
 
   import type { OPCard, OPLang } from '@opcgdb/types';
 
+  import AttributeImage from '~/components/AttributeImage.vue';
   import CardImage from '~/components/CardImage.vue';
   import { useDb } from '~/composables/useDb';
   import { getCardUrl } from '~/utils/getCardUrl';
@@ -39,20 +40,35 @@
 <template>
   <section class="card-details">
     <div class="inner-wrapper">
-      {{ card }}
       <div class="main-info">
         <div class="image col">
           <CardImage :card="card" :width="200" :isLink="false" />
         </div>
         <div class="details col">
-          <h1>{{ card.name }}</h1>
-          <p v-if="card.cost">Cost: {{ card.cost }}</p>
-          <p v-if="card.life">Life: {{ card.life }}</p>
-          <p v-if="card.attribute">Attribute: {{ card.attribute }}</p>
-          <p v-if="card.power !== undefined">Power: {{ card.power }}</p>
-          <p v-if="card.counter">Counter: {{ card.counter }}</p>
-          <p v-if="card.effect">Effect: {{ card.effect }}</p>
-          <p v-if="card.trigger">Trigger: {{ card.trigger }}</p>
+          <h1 class="row name">
+            <span class="name">{{ card.name }}</span>
+            <AttributeImage v-if="card.attribute" :attribute="card.attribute" />
+          </h1>
+
+          <div class="row">
+            <span class="cell" v-if="card.cost">
+              <span class="label">Cost</span> {{ card.cost }}
+            </span>
+            <span class="cell" v-if="card.life">
+              <span class="label">Life</span> {{ card.life }}
+            </span>
+            <span class="cell" v-if="card.power !== undefined">
+              <span class="label">Power</span> {{ card.power }}
+            </span>
+            <span class="cell" v-if="card.counter">
+              <span class="label">Counter</span> {{ card.counter }}
+            </span>
+          </div>
+
+          <p class="row" v-if="card.effect"><span class="label">Effect</span> {{ card.effect }}</p>
+          <p class="row" v-if="card.trigger">
+            <span class="label">Trigger</span> {{ card.trigger }}
+          </p>
         </div>
         <div class="versions col">
           <p>Set: {{ card.set }}</p>
@@ -89,13 +105,108 @@
 </template>
 
 <style scoped>
+  @import '~/styles/media';
+
   .card-details {
     .main-info {
+      padding: var(--spacing-lg) 0;
       display: flex;
       justify-content: space-evenly;
 
+      @media (--mobile) {
+        flex-direction: column;
+      }
+
       .col {
         width: 33%;
+        @media (--mobile) {
+          width: 100%;
+          margin-bottom: var(--spacing-lg);
+        }
+      }
+    }
+
+    .image {
+      .card {
+        @media (--mobile) {
+          max-width: 300px;
+          margin: 0 auto;
+        }
+      }
+    }
+
+    .details {
+      position: relative;
+      top: var(--spacing-lg);
+      left: calc(var(--spacing-md) * -1);
+      border: 1px solid var(--gray);
+      border-top: 4px solid var(--main-blue);
+      border-bottom: 4px solid var(--main-blue);
+      border-radius: 3px;
+      display: flex;
+      flex-direction: column;
+      height: fit-content;
+
+      @media (--mobile) {
+        position: static;
+      }
+
+      p.row {
+        display: block;
+        .label {
+          display: block;
+          margin-bottom: var(--spacing-sm);
+          font-size: var(--font-size-bigbody);
+        }
+      }
+
+      .row {
+        padding: var(--spacing-sm);
+        padding-left: calc(var(--spacing-md) + var(--spacing-sm));
+        border-bottom: 1px solid var(--gray);
+        display: flex;
+        align-items: center;
+        flex-wrap: wrap;
+
+        @media (--mobile) {
+          padding-left: var(--spacing-sm);
+        }
+
+        &:last-child {
+          border-bottom: none;
+        }
+
+        .name {
+          font-weight: 700;
+          font-size: var(--font-size-bigbody);
+        }
+
+        .label {
+          font-weight: 700;
+          display: inline-block;
+          margin-right: var(--spacing-xs);
+
+          @media (--mobile) {
+            width: 50%;
+          }
+        }
+
+        .cell {
+          width: 50%;
+          margin-bottom: var(--spacing-sm);
+          &:last-child {
+            margin-bottom: 0;
+          }
+
+          @media (--mobile) {
+            width: 100%;
+          }
+        }
+
+        &.name {
+          justify-content: space-between;
+          align-items: flex-start;
+        }
       }
     }
   }
